9d62eeaa162954942734cea1fa6ef9452dd8bb4c
Geoffrey Decan <geoffrey.decan@gmail.com>
2025-08-06 17:35:08 +0200

    add logger mock builder implementation and tests

---
+++ src/TestFabric/LoggerMockBuilder{T}.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TestFabric;
+
+public class LoggerMockBuilder<T> : MockBuilder<ILogger<T>>
+{
+    public LoggerMockBuilder<T> WithLog()
+    {
+        WithAction(x => x.Log(
+            It.IsAny<LogLevel>(),
+            It.IsAny<EventId>(),
+            It.IsAny<It.IsAnyType>(),
+            It.IsAny<Exception>(),
+            It.IsAny<Func<It.IsAnyType, Exception, string>>()));
+        return this;
+    }
+
+    public LoggerMockBuilder<T> WithLog(List<string> log)
+    {
+        return WithLog((level, text, ex) =>
+        {
+            text = ex != null
+                ? $"{text} | {ex.Message}"
+                : text;
+            log.Add($"[{level}] {text}");
+        });
+    }
+
+    public LoggerMockBuilder<T> WithLog(Action<LogLevel, string, Exception> handler)
+    {
+        Mock
+            .Setup(x => x.Log(
+                It.IsAny<LogLevel>(),
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()))
+            .Callback(
+                new InvocationAction(invocation =>
+                {
+                    var level = (LogLevel)invocation.Arguments[0];
+                    //var eventId = (EventId)invocation.Arguments[1];
+                    var state = invocation.Arguments[2];
+                    var exception = (Exception)invocation.Arguments[3];
+                    var formatter = invocation.Arguments[4];
+
+                    var invokeMethod = formatter.GetType().GetMethod("Invoke");
+                    var text = (string)invokeMethod?.Invoke(formatter, new[] { state, exception });
+                    handler(level, text, exception);
+                }));
+
+        return this;
+    }
+}
---
+++ tests/TestFabric.Test/LoggerMockBuilderTests.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TestFabric.Test;
+
+public class LoggerMockBuilderTests
+{
+    [Fact]
+    public void Given_Logger_When_LogTrace_Then_Success()
+    {
+        // Arrange
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog()
+            .Create();
+
+        // Act
+        sut.Object.LogTrace("trace");
+
+        // Assert
+        // no error
+    }
+
+    [Fact]
+    public void Given_Logger_When_LogDebug_Then_Success()
+    {
+        // Arrange
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog()
+            .Create();
+
+        // Act
+        sut.Object.LogDebug("debug");
+
+        // Assert
+        // no error
+    }
+
+    [Fact]
+    public void Given_Logger_When_LogInformation_Then_Success()
+    {
+        // Arrange
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog()
+            .Create();
+
+        // Act
+        sut.Object.LogInformation("info");
+
+        // Assert
+        // no error
+    }
+
+    [Fact]
+    public void Given_Logger_When_LogWarning_Then_Success()
+    {
+        // Arrange
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog()
+            .Create();
+
+        // Act
+        sut.Object.LogWarning("warn");
+
+        // Assert
+        // no error
+    }
+
+    [Fact]
+    public void Given_Logger_When_LogError_Then_Success()
+    {
+        // Arrange
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog()
+            .Create();
+
+        // Act
+        sut.Object.LogError("error");
+
+        // Assert
+        // no error
+    }
+
+    [Fact]
+    public void Given_Logger_When_LogCritical_Then_Success()
+    {
+        // Arrange
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog()
+            .Create();
+
+        // Act
+        sut.Object.LogCritical("critical");
+
+        // Assert
+        // no error
+    }
+
+    [Fact]
+    public void Given_LoggerWithLog_When_LogInformation_Then_TextAdded()
+    {
+        // Arrange
+        var log = new List<string>();
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog(log)
+            .Create();
+        const string expected = "[Information] Hello World!";
+
+        // Act
+        sut.Object.LogInformation(
+            "Hello {name}!",
+            "World");
+
+        // Assert
+        Assert.Single(log);
+        Assert.Equal(expected, log.First());
+    }
+
+    [Fact]
+    public void Given_LoggerWithLog_When_LogErrorWithException_Then_TextAdded()
+    {
+        // Arrange
+        var log = new List<string>();
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog(log)
+            .Create();
+        const string expected = "[Error] Hello World! | My exception message";
+
+        // Act
+        sut.Object.LogError(
+            new Exception("My exception message"),
+            "Hello {name}!",
+            "World");
+
+        // Assert
+        Assert.Single(log);
+        Assert.Equal(expected, log.First());
+    }
+
+    [Fact]
+    public void Given_LoggerWithHandler_When_LogErrorWithException_Then_TextAdded()
+    {
+        // Arrange
+        var response = "";
+        var sut = new LoggerMockBuilder<Foo>()
+            .WithLog((l, s, e) =>
+            {
+                response = $"|{l}|{s}|{e?.Message}";
+            })
+            .Create();
+        const string expected = "|Error|Hello World!|My exception message";
+
+        // Act
+        sut.Object.LogError(
+            new Exception("My exception message"),
+            "Hello {name}!",
+            "World");
+
+        // Assert
+        Assert.Equal(expected, response);
+    }
+
+    public class Foo;
+}
