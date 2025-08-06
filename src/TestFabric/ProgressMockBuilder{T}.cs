b8c5a660ff49cacabdbc5646ad43f86e68377bcd
Geoffrey Decan <geoffrey.decan@gmail.com>
2025-08-06 18:29:39 +0200

    add progress mock builder implementation and tests

---
+++ src/TestFabric/ProgressMockBuilder{T}.cs
@@ -0,0 +1,29 @@
+﻿namespace TestFabric;
+
+/// <summary>
+///     Builder for creating mock objects of type <see cref="IProgress{T}" /> with fluent methods.
+/// </summary>
+/// <typeparam name="T">Type parameter for the progress report.</typeparam>
+public class ProgressMockBuilder<T> : MockBuilder<IProgress<T>>
+{
+    /// <summary>
+    ///     Configures the mock to execute a default action when the Report method is called.
+    /// </summary>
+    /// <returns>The current builder instance for fluent chaining.</returns>
+    public ProgressMockBuilder<T> WithReport()
+    {
+        WithAction(x => x.Report(It.IsAny<T>()));
+        return this;
+    }
+
+    /// <summary>
+    ///     Configures the mock to execute a specific action when the Report method is called.
+    /// </summary>
+    /// <param name="callback">The action to be executed when the Report method is called.</param>
+    /// <returns>The current builder instance for fluent chaining.</returns>
+    public ProgressMockBuilder<T> WithReport(Action<T> callback)
+    {
+        WithAction(x => x.Report(It.IsAny<T>()), callback);
+        return this;
+    }
+}
---
+++ tests/TestFabric.Test/ProgressMockBuilderTests.cs
@@ -0,0 +1,35 @@
+﻿namespace TestFabric.Test;
+
+public class ProgressMockBuilderTests
+{
+    [Fact]
+    public void Given_Progress_When_Report_Then_Success()
+    {
+        // Arrange
+        var sut = new ProgressMockBuilder<int>()
+            .WithReport()
+            .Create();
+
+        // Act
+        sut.Object.Report(1);
+
+        // Assert
+        // no error
+    }
+
+    [Fact]
+    public void Given_ProgressCallback_When_Report_Then_Called()
+    {
+        // Arrange
+        var callbackExecuted = false;
+        var sut = new ProgressMockBuilder<int>()
+            .WithReport(_ => callbackExecuted = true)
+            .Create();
+
+        // Act
+        sut.Object.Report(1);
+
+        // Assert
+        Assert.True(callbackExecuted);
+    }
+}
