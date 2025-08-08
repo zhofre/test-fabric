c47c75b60d5107b1a0f1e726f7bb28d6498cc603
Geoffrey Decan <geoffrey.decan@gmail.com>
2025-08-08 17:15:10 +0200

    update person factory sample

--- samples/Sample.Library.MSTest.Test/PersonFactoryTests.cs
+++ samples/Sample.Library.MSTest.Test/PersonFactoryTests.cs
@@ -1,26 +1,29 @@
 ﻿using Sample.Library.TestTools;
+using TestFabric;
 
 namespace Sample.Library.MSTest.Test;
 
 [TestClass]
-public sealed class PersonFactoryTests
+public sealed class PersonFactoryTests : TestSuite.Normal
 {
     [TestMethod]
     public void Given_IdAndName_When_Create_Then_IdAndNameAsProperty()
     {
         // Arrange
-        var id = Guid.NewGuid();
-        const string name = "John";
+        var id = Factory.Create<Guid>();
+        var name = Factory.Create<string>();
+        var age = Factory.CreateFromRange(16, 82);
         var guidGeneratorMock = new GuidGeneratorMockBuilder()
             .WithGenerate(id)
             .Create();
         var sut = new PersonFactory(guidGeneratorMock.Object);
 
         // Act
-        var actual = sut.Create(name);
+        var actual = sut.Create(name, age);
 
         // Assert
         Assert.AreEqual(id, actual.Id);
         Assert.AreEqual(name, actual.Name);
+        Assert.AreEqual(age, actual.Age);
     }
 }
--- samples/Sample.Library.xUnit.Test/PersonFactoryTests.cs
+++ samples/Sample.Library.xUnit.Test/PersonFactoryTests.cs
@@ -1,25 +1,28 @@
 ﻿using Sample.Library.TestTools;
+using TestFabric;
 
 namespace Sample.Library.xUnit.Test;
 
-public class PersonFactoryTests
+public class PersonFactoryTests : TestSuite.Normal
 {
     [Fact]
     public void Given_IdAndName_When_Create_Then_IdAndNameAsProperty()
     {
         // Arrange
-        var id = Guid.NewGuid();
-        const string name = "John";
+        var id = Factory.Create<Guid>();
+        var name = Factory.Create<string>();
+        var age = Factory.CreateFromRange(16, 82);
         var guidGeneratorMock = new GuidGeneratorMockBuilder()
             .WithGenerate(id)
             .Create();
         var sut = new PersonFactory(guidGeneratorMock.Object);
 
         // Act
-        var actual = sut.Create(name);
+        var actual = sut.Create(name, age);
 
         // Assert
         Assert.Equal(id, actual.Id);
         Assert.Equal(name, actual.Name);
+        Assert.Equal(age, actual.Age);
     }
 }
