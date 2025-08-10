f693b6466e6c2a8fbf648332ea1897f2b9a4568f
Geoffrey Decan <geoffrey.decan@gmail.com>
2025-08-10 23:09:27 +0200

    add simple record tests to sample

---
+++ samples/Sample.Library.xUnit.Test/PersonTests.cs
@@ -0,0 +1,34 @@
+﻿using TestFabric;
+
+namespace Sample.Library.xUnit.Test;
+
+public class PersonTests : TestSuite.Normal
+{
+    [Fact]
+    public void Given_Nothing_When_Random_Then_PersonWithData()
+    {
+        // Arrange
+
+        // Act
+        var person = Random<Person>();
+
+        // Assert
+        Assert.NotNull(person);
+        Assert.NotEqual(Guid.Empty, person.Id);
+    }
+
+    [Fact]
+    public void Given_Id_When_BuildWithId_Then_PersonWithId()
+    {
+        // Arrange
+        var id = Random<Guid>();
+
+        // Act
+        var person = Factory.Build<Person>()
+            .With(x => x.Id, id)
+            .Create();
+
+        // Assert
+        Assert.Equal(id, person.Id);
+    }
+}
